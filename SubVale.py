1132821c38509fe75ca33f6c7d811acc87a68310
Joseph Kato <joseph@jdkato.io>
2017-02-23 16:55:04 -0800

    chore: add project

---
+++ SubVale.py
@@ -0,0 +1,127 @@
+import json
+
+import sublime_plugin
+import sublime
+
+from .util.settings import Settings
+from .util import util
+
+
+def plugin_loaded():
+    """Load plugin settings.
+    """
+    Settings.load()
+
+
+class ValeCommand(sublime_plugin.TextCommand):
+    """Manages Vale's linting functionality.
+    """
+    def run(self, edit):
+        """Run vale on the user-indicated buffer.
+        """
+        syntax = self.view.settings().get('syntax')
+
+        # Verify that the binary exists and that the syntax is supported.
+        if not Settings.vale_exists():
+            print('The vale binary was not found.')
+            return
+        elif not util.get_is_supported(syntax):
+            print('Syntax not supported; skipping...')
+            return
+
+        encoding = self.view.encoding()
+        if encoding == 'Undefined':
+            encoding = 'utf-8'
+
+        path = self.view.file_name()
+        cmd = [Settings.get('binary'), '--output=JSON', path]
+        buf = self.view.substr(sublime.Region(0, self.view.size()))
+        output, error = util.pipe_through_prog(cmd, buf.encode(encoding), path)
+        if error:
+            sublime.error_message('Vale: ' + error.decode('utf-8'))
+            return
+        self.show_alerts(json.loads(output.decode(encoding)))
+
+    def show_alerts(self, data):
+        """Add alert regions to the view.
+        """
+        Settings.clear_on_hover()
+        regions = []
+        for f, alerts in data.items():
+            for a in alerts:
+                start = self.view.text_point(a['Line'] - 1, 0)
+                loc = (start + a['Span'][0] - 1, start + a['Span'][1])
+                regions.append(sublime.Region(*loc))
+                Settings.on_hover.append({
+                    'region': regions[-1], 'HTML': self._make_content(a),
+                    'view': self.view.id, 'level': a['Severity'],
+                    'msg': a['Message']
+                })
+        self.view.add_regions('Vale-Alerts', regions,
+                              Settings.get('highlight_scope'),
+                              Settings.get('icon'),
+                              Settings.get_draw_style())
+
+    def _make_content(self, alert):
+        """Convert an alert into HTML suitable for a popup.
+        """
+        level = alert['Severity'].capitalize()
+        if level == 'Error':
+            html = sublime.load_resource('Packages/Vale/static/error.html')
+        elif level == 'Warning':
+            html = sublime.load_resource('Packages/Vale/static/warning.html')
+        else:
+            html = sublime.load_resource('Packages/Vale/static/info.html')
+
+        source = alert['Link']
+        if source != '':
+            source = util.make_link(source, 'Read more ...')
+
+        if alert['Description'] == '':
+            header = '{}: {}'.format(level, alert['Check'])
+            body = alert['Message']
+        else:
+            header = '{}: {}'.format(level, alert['Message'])
+            body = alert['Description']
+
+        content = html.format(
+            CSS=sublime.load_resource('Packages/Vale/static/ui.css'),
+            header=header, body=body, source=source)
+        return content
+
+
+class ValeEventListener(sublime_plugin.EventListener):
+    """Monitors event related to Vale.
+    """
+    def on_modified_async(self, view):
+        """Clear the view of Vale's regions.
+        """
+        Settings.clear_on_hover()
+        view.erase_regions('Vale-Alerts')
+
+    def on_pre_save_async(self, view):
+        """Run Vale on the entire buffer on file save.
+        """
+        syntax = view.settings().get('syntax')
+        if util.get_is_supported(syntax):
+            if Settings.get('format_on_save'):
+                print('Auto-applying Vale on save...')
+                view.run_command('vale')
+
+    def on_hover(self, view, point, hover_zone):
+        """Show an alert.
+        """
+        if not util.get_is_supported(view.settings().get('syntax')):
+            return
+
+        loc = Settings.get('alert_location')
+        for alert in Settings.on_hover:
+            if alert['view'] == view.id and alert['region'].contains(point):
+                if loc == 'hover_popup':
+                    view.show_popup(
+                        alert['HTML'], flags=sublime.HIDE_ON_MOUSE_MOVE_AWAY,
+                        location=point, max_width=450,
+                        on_navigate=util.open_link)
+                elif loc == 'hover_status_bar':
+                    sublime.status_message(
+                        'vale:{0}:{1}'.format(alert['level'], alert['msg']))
---
+++ util/settings.py
@@ -0,0 +1,137 @@
+import json
+import shutil
+import subprocess
+
+import sublime
+
+SUPPORTED = [
+    'HTML', 'Markdown', 'Plain Text', 'Markdown GFM', 'reStructuredText',
+    'reStructuredText Improved', 'Asciidoc', 'Asciidoc (AsciiDoctor)'
+]
+
+
+class ValeSettings:
+    """Provide global access to and management of Clang Format's settings.
+    """
+    def __init__(self):
+        self.supported = SUPPORTED
+        self.settings_file = 'Vale.sublime-settings'
+        self.default_binary = 'vale'
+        if sublime.platform() == 'windows':
+            self.default_binary += '.exe'
+        self.settings = {}
+        self.on_hover = []
+
+    def load(self):
+        """Load vale's settings.
+        """
+        self.settings = sublime.load_settings(self.settings_file)
+        self.prefs = sublime.load_settings(self.pref_file)
+        self.settings.add_on_change('reload', lambda: self.load())
+        self.prefs.add_on_change('reload', lambda: self.load())
+
+    def vale_exists(self):
+        """Determine if the vale binary exists.
+
+        Returns:
+            bool: True if the check was successful and False otherwise.
+        """
+        msg = 'The vale binary was not found. Do you want to set a new path?'
+        # If we couldn't find the binary.
+        if shutil.which(self.get('binary')) is None:
+            # Try to guess the correct setting.
+            path = shutil.which(self.default_binary)
+            if path:
+                # Looks like vale is in the path, remember that.
+                self.set('binary', path)
+            elif sublime.ok_cancel_dialog(msg):
+                self._update_binary_path()
+            return shutil.which(self.get('binary'))
+        return True
+
+    def get_styles(self):
+        """Get Vale's base styles.
+        """
+        config = self._get_config()
+        return config['GBaseStyles']
+
+    def get_draw_style(self):
+        """Get the region styling.
+        """
+        underlined = sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE
+        style = self.get('alert_style')
+        if style == 'solid_underline':
+            return sublime.DRAW_SOLID_UNDERLINE | underlined
+        elif style == 'stippled_underline':
+            return sublime.DRAW_STIPPLED_UNDERLINE | underlined
+        elif style == 'squiggly_underline':
+            return sublime.DRAW_SQUIGGLY_UNDERLINE | underlined
+        return sublime.DRAW_OUTLINED
+
+    def _get_config(self):
+        """Create a list of settings from the vale binary.
+        """
+        if not self.vale_exists():
+            return {}
+
+        binary = self.get('binary')
+        startupinfo = None
+        if sublime.platform() == 'windows':
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+        command = [binary, 'dump-config']
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             startupinfo=startupinfo)
+        output, error = p.communicate()
+
+        return json.loads(output.decode('utf-8'))
+
+    def _update_binary_path(self):
+        """Update the path to the Clang Format binary.
+        """
+        w = sublime.active_window()
+        caption = 'Path to vale: '
+        on_done = lambda path: self.set('binary', path)
+        w.show_input_panel(caption, self.get('binary'), on_done, None, None)
+
+    def set(self, setting, value, pref=False):
+        """Store and save `setting` as `value`.
+
+        Args:
+            setting (str): The name of the setting to be accessed.
+            value (str, int, bool): The value to be stored.
+            pref (bool, optional): Indicates whether `value` should be stored
+                in the settings or custom settings file.
+        """
+        if pref:
+            self.prefs.set(setting, value)
+            f = self.pref_file
+        else:
+            self.settings.set(setting, value)
+            f = self.settings_file
+        sublime.save_settings(f)
+
+    def get(self, setting, pref=False):
+        """Return the value associated with `setting`.
+
+        Args:
+            setting (str): The name of the setting to be accessed.
+            pref (bool, optional): Indicates whether `value` should be stored
+                in the settings or custom settings file.
+        Returns:
+            (str, int, bool): The value associated with `setting`. The default
+                value is None.
+        """
+        if pref:
+            return self.prefs.get(setting, None)
+        else:
+            return self.settings.get(setting, None)
+
+    def clear_on_hover(self):
+        """
+        """
+        del self.on_hover[:]
+
+Settings = ValeSettings()
